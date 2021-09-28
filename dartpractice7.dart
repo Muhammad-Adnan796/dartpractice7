b9b5ca0c57da3eb80a26f814cd54f00133de19d6
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-28 05:17:20 -0700

    Add files via upload

---
+++ dartpractice7.dart
@@ -0,0 +1,96 @@
+void main() {
+//   var a = 12;
+//   var b = 23.4;
+// //   var c = a+b;
+// //   print (c);
+// //   bool result = a<b;
+// //   print (result);
+//   if(a<b && a==b){
+//     print ('true');
+//   }else{
+//     print ('false');
+//   }
+ 
+  
+//   var a1 = 35;
+//   var a2 = 30;
+//   if(a1!=a2 && a1>a2){
+//     print ('true');
+//   }else{
+//     print ('false');
+//   }
+
+  
+//   var a1 = 30;
+//   var a2 = 40;
+//   if(a1!=a2 && a1<a2){
+//     print ('true');
+//   }else{
+//     print ('false');
+//   }
+  
+//   var a1 = [1,2,3,4,5,6,7,8];
+// //   var a2 = [2,4,6,8,10];
+// //   var a2 = a1.first;
+// //   print('this is natural number $a2');
+// var a3 = a1.last;
+//   print ('this is an even number $a3');
+//   var a4 = a1.length;
+//   print (a4);
+//   var a5 = a1.elementAt(0);
+//   print(a5);
+  
+//    var a1 = [10,11,12,3,4,5,6,7,8];
+//   a1[0]=3;
+//   print(a1);
+  
+// a1[4] = 9;
+//   print(a1);
+  
+//   a1.replaceRange(1,2,[3]);
+//   print(a1);
+//    a1.replaceRange(1,4,[10,11,12]);
+//   print(a1);
+//   a1.sort();
+//   print(a1);
+//   if( a1.isNotEmpty){
+//     print ('list is empty');
+//   }else{
+//     print ('list is not empty');
+//   }
+  
+  
+  
+//        var a1 = [10,11,12,3,4,5,6,7,8];
+//   var a1 = [];
+//   a1.add(1);
+//   a1.add(2);
+//   a1.add(3);
+  
+// //   a1.clear();
+//     if( a1.isEmpty){
+//     print ('list is empty');
+//   }else{
+//     print ('list is not empty');
+//   }
+  
+//   var a1 = [10,11,12,3,4,5,6,7,8];
+// //   var a2 =List.of(a1.reversed);
+//   print(List.of(a1.reversed));
+  
+//   var a1 = ['adnan','zeeshan','yasir','sheraz','hamza'];
+// //   var a2 = List.of(a1.reversed);
+// //   print(a2);
+//   print (List.of(a1.reversed));
+  
+//   var a = [1,2,3,4,5,6];
+//   a.add(2);
+//   a.add(4);
+//   a.addAll([1,2,5]);
+//   print (a);
+  
+//   var a1 = ['shehzad','jansher','zeeshan'];
+//    a1.insert(0,'zeeshan');
+//   a1.insertAll(0,['adnan','sheraz']);
+//   print (a1);
+  }
